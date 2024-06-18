d752196822d29b4a5fcb65895847d302c936de3f
Shruti Agarwal1 <shruti_agarwal1@epam.com>
2024-06-18 17:29:16 +0530

    microservices added

---
+++ HotelService/src/main/java/com/example/HotelService/controllers/StaffController.java
@@ -0,0 +1,22 @@
+package com.example.HotelService.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/staffs")
+public class StaffController {
+
+    @GetMapping
+    public ResponseEntity<List<String>> getStaffDetails()
+    {
+        List<String> list = Arrays.asList("Ram", "Shyam", "Sita", "Geeta");
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+}
